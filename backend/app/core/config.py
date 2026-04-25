5cac923d42675331dca4ac47ad60fe6a82159aee
m.shamir <m.shamir@aman.com.sa>
2026-04-25 16:46:27 +0300

    fix

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -40,7 +40,8 @@ class Settings(BaseSettings):
     NVD_API_KEY: str = ""
     
     class Config:
-        env_file = ".env"
+        env_file = ".env",
+        extra = "ignore"
 
 
 settings = Settings()
